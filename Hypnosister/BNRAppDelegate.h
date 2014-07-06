465bdfe3bb3ce562fcc70574a09da0c358da3746
Steffan Voges <steffan@berkeley.edu>
2014-07-06 12:58:55 -0700

    Initial Commit

---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Steffan Voges on 7/6/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Steffan Voges on 7/6/14.
+//  Copyright (c) 2014 Steffan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
